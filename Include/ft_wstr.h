53c3eb397f1f40e5dbbdf3939d13a944a9503b83
Carnagiul-MAC <pierrequeruel@gmail.com>
2018-05-18 08:59:35 +0200

    Update norme project

--- Include/ft_wstr.h
+++ Include/ft_wstr.h
@@ -20,18 +20,18 @@ typedef struct	s_unix_char_limit
 	char		*name;
 }				t_unix_char_limit;
 
-size_t		ft_wcharlen(int wc);
-size_t		ft_wstrlen(wchar_t *str);
-void		ft_putwstr(const wchar_t *str, t_print *print);
-char		*ft_unix_string_get_name(int wc);
-int			ft_unix_char_is_valid(int wc);
-void		ft_unix_string_display_put_struct(t_unix_char_limit l);
-void		ft_unix_string_display_put(void);
-int			ft_unix_string_display_ii(int wc);
-int			ft_unix_string_display_iii(int wc);
-int			ft_unix_string_display_iv(int wc);
-int			ft_unix_char_is_display_ii(int wc);
-int			ft_unix_char_is_display_iii(int wc);
-int			ft_unix_char_is_display_iv(int wc);
+size_t			ft_wcharlen(int wc);
+size_t			ft_wstrlen(wchar_t *str);
+void			ft_putwstr(const wchar_t *str, t_print *print);
+char			*ft_unix_string_get_name(int wc);
+int				ft_unix_char_is_valid(int wc);
+void			ft_unix_string_display_put_struct(t_unix_char_limit l);
+void			ft_unix_string_display_put(void);
+int				ft_unix_string_display_ii(int wc);
+int				ft_unix_string_display_iii(int wc);
+int				ft_unix_string_display_iv(int wc);
+int				ft_unix_char_is_display_ii(int wc);
+int				ft_unix_char_is_display_iii(int wc);
+int				ft_unix_char_is_display_iv(int wc);
 
 #endif
