23ac002623fec9625dabe077739b9fe746b9a19e
Pierre QUERUEL <piquerue@e3r4p19.42.fr>
2018-05-19 00:13:24 +0200

    update norme

--- main.c
+++ main.c
@@ -1,65 +1,73 @@
 #include "libft.h"
 
-void    set_file(t_lem *lem)
+static int			set_file_get_command(char *cmd)
 {
-	int i;
-	int type;
-	int tube;
-	int done;
-	char *rk;
+	if (ft_strcmp(cmd, "##start") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "##end") == 0)
+		return (2);
+	return (0);
+}
 
-	tube = 0;
-	i = -1;
-	type = 0;
-	done = 0;
+static int			set_file_set_ants(char *cmd, t_lem *lem)
+{
+	char	*rk;
+
+	lem->atns = ft_atoi(cmd);
+	rk = ft_itoa(lem->atns);
+	if (ft_strcmp(rk, cmd) != 0 || lem->atns <= 0)
+		exit(0);
+	ft_strdel(&rk);
+	return (1);
+}
+
+static int			set_file_set_tube(char *l, int tube, t_lem *lem, int type)
+{
+	if (type >= 0)
+	{
+		if (tube == 1)
+			add_tube(l, &(lem->rooms));
+		else
+			add_room(set_room(l, type), &(lem->rooms));
+		if (type == 2)
+			tube = 1;
+	}
+	return (tube);
+}
+
+static int			set_file_exist(t_lem *lem)
+{
 	if (!lem->filecontents)
 		exit(0);
+	return (0);
+}
+
+void		set_file(t_lem *lem)
+{
+	int		i;
+	int		type;
+	int		tube;
+	int		done;
+
+	i = -1;
+	type = 0;
+	tube = 0;
+	done = set_file_exist(lem);
 	while (lem->filecontents[++i])
-	{
 		if (ft_strlen(lem->filecontents[i]) == 0)
 			return ;
-		if (lem->filecontents[i][0] == '#')
-		{
-			if (ft_strcmp(lem->filecontents[i], "##start") == 0)
-				type = 1;
-			if (ft_strcmp(lem->filecontents[i], "##end") == 0)
-				type = 2;
-		}
+		else if (lem->filecontents[i][0] == '#')
+			type = set_file_get_command(lem->filecontents[i]);
 		else
 		{
-			if (done == 0)
-			{
-				if (type == 0)
-				{
-					lem->atns = ft_atoi(lem->filecontents[i]);
-					rk = ft_itoa(lem->atns);
-					if (ft_strcmp(rk, lem->filecontents[i]) != 0 || lem->atns <= 0)
-						exit(0);
-					ft_strdel(&rk);
-					done = 1;
-				}
-				else
-					exit(0);
-			}
+			if (done == 0 && type == 0)
+				done = set_file_set_ants(lem->filecontents[i], lem);
+			else if (done == 0 && type == 1)
+				exit(0);
 			else
-			{
-				if (type >= 0)
-				{
-					if (tube == 1)
-					{
-						add_tube(lem->filecontents[i], &(lem->rooms));
-					}
-					else
-					{
-						add_room(set_room(lem->filecontents[i], type), &(lem->rooms));
-					}
-					if (type == 2)
-						tube = 1;
-				}
-			}
+				tube = set_file_set_tube(lem->filecontents[i], tube, lem, type);
 			type = 0;
 		}
-	}
 }
 
 void move_ants(t_lem *lem)
