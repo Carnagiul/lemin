05b082020e8f52da70d5197de7cf506caf75b48b
Carnagiul-MAC <pierrequeruel@gmail.com>
2018-05-18 08:43:58 +0200

    Update project Norme

--- Include/ft_lemin.h
+++ Include/ft_lemin.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lemin.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: piquerue <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/18 20:32:05 by piquerue          #+#    #+#             */
+/*   Updated: 2018/05/10 14:52:17 by piquerue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FT_LEMIN_H
 # define FT_LEMIN_H
@@ -39,18 +50,18 @@ typedef struct				s_lem
 	struct s_tube			*path;
 }							t_lem;
 
-t_room				*get_start(t_listroom **lst);
-t_room				*get_end(t_listroom **lst);
-t_room				*set_room(char *str, int type);
-void				tube_set(t_tube *tube, t_room *room);
-void				insert_tube(t_room *a, t_room *b);
-void				display_room_data(t_room *room);
-void				display_room_link(t_room *room);
-void				display_room_and_tubes(t_lem *lem);
-t_tube				*get_first_min(t_tube **tube, t_tube *f);
-t_tube				*create_quick_path(t_room *room);
-void				display_path(t_tube *tb);
-void				set_dist_room(t_room *room, int dist);
-void				add_tube(char *str, t_listroom **tmp);
+t_room						*get_start(t_listroom **lst);
+t_room						*get_end(t_listroom **lst);
+t_room						*set_room(char *str, int type);
+void						tube_set(t_tube *tube, t_room *room);
+void						insert_tube(t_room *a, t_room *b);
+void						display_room_data(t_room *room);
+void						display_room_link(t_room *room);
+void						display_room_and_tubes(t_lem *lem);
+t_tube						*get_first_min(t_tube **tube, t_tube *f);
+t_tube						*create_quick_path(t_room *room);
+void						display_path(t_tube *tb);
+void						set_dist_room(t_room *room, int dist);
+void						add_tube(char *str, t_listroom **tmp);
 
 #endif
